d4372a91da4e709d06ed8261e50c2a0bd74d9724
Sergio Perez <se.perezr12@uniandes.edu.co>
2025-05-18 18:26:09 -0500

    Simulacion de error en new relic

--- src/blueprints/routes.py
+++ src/blueprints/routes.py
@@ -6,6 +6,13 @@ from errors.errors import ApiError
 
 main = Blueprint("main", __name__)
 
+
+@main.route("/error", methods=(['GET']))
+def trigger_error():
+    raise Exception("Este es un error simulado para New Relic")
+
+
+
 @main.route("/ping", methods=(['GET']))
 def ping():
   return {"msg": "Solo para confirmar que el servicio está arriba."}, 200
